2745db19f89571ec3c042558cde1c7a821d5cae3
noormohammad20 <mdsaim204@gmail.com>
2022-03-26 14:54:16 +0600

    added answer of questions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import Answer from './components/Answer/Answer'
 import Header from './components/Header/Header'
 import Site from './components/Site/Site'
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header></Header>
       <Site></Site>
+      <Answer></Answer>
     </div>
   )
 }
